43608fcc55381806247f3d561b782cb9d7d787e4
aarathybabu97 <aarathybabu907@gmail.com>
2024-11-05 17:44:50 +1100

    Load libraries

--- exercises/1-code/1-clean-self-assessed-health.R
+++ exercises/1-code/1-clean-self-assessed-health.R
@@ -1,16 +1,18 @@
 ###############################################################
-# Project:
-# Purpose:
-# Inputs:
-# Outputs:
-# Author:
+# Project: Data cleaning
+# Purpose: Clean National Health Survey Data
+# Inputs: exercises/2-raw-data/national-health-survey-2022-table-2.xlsx
+# Outputs: exercises/3-clean-data/vis1.Rda
+# Author: Aarathy Babu
 ###############################################################
 
 ##################
 # Load libraries #
 ##################
 
-
+library(readxl)
+library(dplyr)
+library(tidyr)
 
 #################
 # Read the data #
@@ -27,6 +29,3 @@
 ###################
 # Save the charts #
 ###################
-
-
-
