72febc5a93b95661dcee60926446472c8801135a
aarathybabu97 <aarathybabu907@gmail.com>
2024-11-07 12:17:00 +1100

    clean chronic conditions

--- exercises/1-code/2-clean-chronic-conditions.R
+++ exercises/1-code/2-clean-chronic-conditions.R
@@ -19,7 +19,7 @@ library(tidyr)
 url <- "https://www.abs.gov.au/statistics/health/health-conditions-and-risks/national-health-survey/2022/NHSDC01.xlsx"
 
 # Download the file locally
-download.file(url, 'solutions/2-raw-data/national-health-survey-2022-table-1.xlsx')
+download.file(url, mode="wb", 'solutions/2-raw-data/national-health-survey-2022-table-1.xlsx')
 
 # Read in the raw data
 df2Raw <- read_excel(
